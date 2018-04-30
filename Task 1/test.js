d83c91bf3feabb96ae83a6baa9b019b38a9c0209
Jens Sproede <j.sproede@appcom-interactive.de>
2018-04-30 08:54:05 +0200

    Fix issues with test, wrong usage of assert library

--- Task 1/test.js
+++ Task 1/test.js
@@ -1,34 +1,34 @@
 // Replace `template` with your own solution
-const polybios = require('./solutions/template');
+const polybios = require('./solutions/jens');
 
 var mocha = require('mocha');
 var describe = mocha.describe;
 var it = mocha.it;
 
-const assert = require('assert');
+const expect = require('chai').expect;
 
 describe('your polybios solution', () => {
     it('should return the decrypted value for HALLO', () => {
-        assert(polybios.encrypt('HALLO'), '23 11 31 31 34');
+        expect(polybios.encrypt('HALLO')).to.eq('23 11 31 31 34');
     });
 
     it('should return the decrypted value for APPCOM', () => {
-        assert(polybios.encrypt('APPCOM'), '11 35 35 13 34 32');
+        expect(polybios.encrypt('APPCOM')).to.eq('11 35 35 13 34 32');
     });
     
     it('should return the decrypted value for BAT', () => {
-        assert(polybios.encrypt('BAT'), '11 35 35 13 34 32');
+        expect(polybios.encrypt('BAT')).to.eq('12 11 44');
     });
     
     it('should return the decrypted value for FLUGZEUG', () => {
-        assert(polybios.encrypt('FLUGZEUG'), '21 31 45 22 55 15 45 22');
+        expect(polybios.encrypt('FLUGZEUG')).to.eq('21 31 45 22 55 15 45 22');
     });
 
     it('should return the decrypted value for WEBDEV', () => {
-        assert(polybios.encrypt('WEBDEV'), '52 15 12 14 15 51');
+        expect(polybios.encrypt('WEBDEV')).to.eq('52 15 12 14 15 51');
     });
 
     it('should return the decrypted value for INTERNET', () => {
-        assert(polybios.encrypt('INTERNET'), '24 33 44 15 42 33 15 44');
+        expect(polybios.encrypt('INTERNET')).to.eq('24 33 44 15 42 33 15 44');
     });
 });
