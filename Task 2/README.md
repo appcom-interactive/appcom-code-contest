4a7f3ed9d240a134503f2b112ed1abb6cdc9f0e1
René Heinen <r.heinen@appcom-interactive.de>
2018-05-07 16:01:06 +0200

    add task 2

---
+++ Task 2/README.md
@@ -0,0 +1,18 @@
+# Der Polybios Chiffre
+
+https://de.wikipedia.org/wiki/Substitution_(Kryptographie)
+https://de.wikipedia.org/wiki/Alphabet_(Kryptologie)
+
+
+## Aufgabe 1
+
+Überlege dir einen Algorithmus, der bereits durch Substitution verschlüsselte Zeichenketten anhand deines Alphabet-Datenmodells entschlüsselt.
+Dabei soll Groß- und Kleinschreibung beibehalten werden. Sofern ein Buchstabe nicht im Mapping auftaucht soll er nicht substituiert werden und einfach in die Ausgabe geschrieben werden. Zudem soll angegeben werden können, welches Mapping genutzt wird.
+
+## Vorgehensweise
+
+Kopiere die template.js Datei und benenne sie um (bspw. dein Name). Diese kannst du dann ins GIT-Repo pushen.
+
+## Tests
+
+Führe Tests mit `npm test` aus. Dadurch wird sichergestellt, dass dein Algorithmus später auch korrekt funktioniert ;)
---
+++ Task 2/test.js
@@ -0,0 +1,30 @@
+// Replace `template` with your own solution
+const substitution = require('./solutions/template');
+
+var mocha = require('mocha');
+var describe = mocha.describe;
+var it = mocha.it;
+
+const expect = require('chai').expect;
+
+describe('your substitution solution', () => {
+  it('should return the decrypted value for Rn0n3tonvo?', () => {
+    expect(substitution.decrypt('Rn0n3tonvo?', 1)).to.eq('Geheimtext?');
+  });
+
+  it('should return the decrypted value for abCDefgh', () => {
+    expect(substitution.decrypt('abCDefgh', 2)).to.eq('76QRdet2');
+  });
+
+  it('should return the decrypted value for appCoM', () => {
+    expect(substitution.decrypt('appCoM', 2)).to.eq('7mmQg9');
+  });
+
+  it('should return the decrypted value for KäsE', () => {
+    expect(substitution.decrypt('KäsE', 2)).to.eq('LäoR');
+  });
+
+  it('should return the decrypted value for KeKsDOSe', () => {
+    expect(substitution.decrypt('KeKsDOSe', 1)).to.eq('KeKsDTSe');
+  });
+});
