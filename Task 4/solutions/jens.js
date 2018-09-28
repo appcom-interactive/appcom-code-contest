ce75e9fb17d35a039ea11a17a20cc8d41eaa3ebd
Jens Sproede <j.sproede@appcom-interactive.de>
2018-09-28 11:44:31 +0200

    refactor jens solution

--- Task 4/solutions/jens.js
+++ Task 4/solutions/jens.js
@@ -1,111 +1,62 @@
-const messages = {
-  fail: {
-    cheater: "Cheater! Cheater! Cheater!",
-    wrong_coin: "Go away! Strange kid!"
-  }
-};
-
-const allowedValues = [null, "A", "B"];
-
-const countOccurrences = (value, coin) => {
-  return value.reduce(
-    (acc, val) => (acc += val.filter(v => v === coin).length),
-    0
-  );
-};
-
-const checkForInvalidCoins = board =>
-  board.some(row => row.some(col => !allowedValues.includes(col)));
-
-const invalidBoard = value => {
-  const playerCoinsA = countOccurrences(value, "A");
-  const playerCoinsB = countOccurrences(value, "B");
-
-  if (playerCoinsB > playerCoinsA) {
-    return messages.fail.cheater;
-  }
-
-  if (checkForInvalidCoins(value)) {
-    return messages.fail.wrong_coin;
-  }
-
-  return false;
-};
-
-const validateArray = (row, coin) =>
+const validate = (row, coin) =>
   row.reduce((count, value) => {
     if (count < 4) {
-      if (value === coin) {
-        count++;
-      } else {
-        count = 0;
-      }
+      value === coin ? count++ : (count = 0);
     }
     return count;
-  }, 0)
-    ? coin
-    : false;
+  }, 0) && coin;
 
-const check = (value, x, y, coin) => {
-  if (coin !== null) {
-    return (
-      validateArray(value[y], coin) || // Check horizontal
-      vertical(value, x, y, coin) ||
-      diagonal(value, x, y, coin)
-    );
-  }
-
-  return false;
-};
-
-const vertical = (value, x, y, coin) => {
-  const col = value
-    .slice() // Copy array
-    .splice(y) // Just get sub-array
-    .reduce((acc, row) => {
-      acc.push(row[x]);
-      return acc;
-    }, []); // Create a new row out of the col values
-
-  return validateArray(col, coin);
-};
-
-const diagonal = (value, x, y, coin) => {
-  const diagonal = value
-    .slice()
-    .splice(y)
-    .reduce(
-      (acc, val) => {
-        acc.left.push(val[x - acc.left.length]);
-        acc.right.push(val[x + acc.right.length]);
-        return acc;
-      },
-      { left: [], right: [] }
-    );
+const check = (board, x, y, coin) => {
+  if (coin === null) return false;
 
   return (
-    validateArray(diagonal.left, coin) || validateArray(diagonal.right, coin)
+    validate(board[y], coin) ||
+    validate(board.map(row => row[x]), coin) ||
+    diagonal(board.slice().splice(y), x, coin)
   );
+};
+
+const diagonal = (board, x, coin) => {
+  const diagonal = board.reduce(
+    (acc, row) => {
+      acc.left.push(row[x - acc.left.length]);
+      acc.right.push(row[x + acc.right.length]);
+      return acc;
+    },
+    { left: [], right: [] }
+  );
+
+  return validate(diagonal.left, coin) || validate(diagonal.right, coin);
+};
+
+const countCoins = (board, coin) =>
+  board.reduce(
+    (coins, row) => (coins += row.filter(value => value === coin).length),
+    0
+  );
+
+const messages = {
+  cheater: "Cheater! Cheater! Cheater!",
+  wrong_coin: "Go away! Strange kid!"
 };
 
 module.exports = {
-  checkGame(value) {
-    // Need to reverse the array to fix an failing test (line 52 onwards)
-    const reversed = value.slice().reverse();
-
-    if ((message = invalidBoard(reversed))) {
-      return message;
+  checkGame(board) {
+    if (countCoins(board, "B") > countCoins(board, "A")) {
+      return messages.cheater;
     }
 
-    const player = reversed.reduce((acc, row, y) => {
-      if (acc) return acc;
-      return row.find((coin, x) => check(reversed, x, y, coin));
-    }, undefined);
-
-    if (player) {
-      return `Player ${player} wins!`;
+    if (board.some(row => row.some(col => ![null, "A", "B"].includes(col)))) {
+      return messages.wrong_coin;
     }
 
-    return messages.fail.cheater;
+    const reversed = board.reverse();
+    const player = reversed.reduce(
+      (result, row, y) =>
+        row.find((coin, x) => check(reversed, x, y, coin)) || result,
+      undefined
+    );
+
+    return player ? `Player ${player} wins!` : messages.cheater;
   }
 };
